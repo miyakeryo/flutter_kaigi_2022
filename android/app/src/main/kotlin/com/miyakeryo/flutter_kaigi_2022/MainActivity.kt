1594ee9391343e494e4aed289ee67476721b230b
みやけりょう <miyakeryo@10x.co.jp>
2022-09-21 13:36:29 +0900

    create flutter app

---
+++ android/app/src/main/kotlin/com/miyakeryo/flutter_kaigi_2022/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.miyakeryo.flutter_kaigi_2022
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
